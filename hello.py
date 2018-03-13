d19c4e2ed8c9573b462a402a2902f02da1c5d070
Chris Parker <chparker@atlassian.com>
2018-03-13 13:25:04 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, send_from_directory
+from flask import url_for
+
+app = Flask(__name__, static_url_path='/atlassian-connect', static_folder='static')
+
+@app.route("/")
+def status():
+    return "I am alive!"
+
+@app.route("/helloworld.html")
+def action():
+    return send_from_directory(app.static_folder, 'jiraCat.html')
+
+@app.route("/atlassian-connect")
+def descriptor():
+    return render_template('atlassian-connect.json')
+    #return url_for('static', filename='atlassian-connect.json')
+
+if __name__ == "__main__":
+    app.run(debug=True)
